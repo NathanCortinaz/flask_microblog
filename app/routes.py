e4486b0a303e165c1a4d06b6391ab4ac4daac8bf
Nathan Willi de Souza Cortinaz <nathancortinaz@hotmail.com>
2021-09-29 14:37:50 -0300

    Cap2 done

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello, World!"
+  user = {'username': "Nathan"}
+  posts= [
+    {
+      'author': {'username': 'Romeu'},
+      'body': 'Au au!'
+    },
+    {
+      'author': {'username': 'Fran'},
+      'body': 'Eu sou a bileia!'
+    },
+    {
+      'author': {'username': 'Leandro'},
+      'body': 'Só por levantar um muro!'
+    }
+  ]
+  return render_template('index.html', title='Página Inicial', user=user, posts=posts)
